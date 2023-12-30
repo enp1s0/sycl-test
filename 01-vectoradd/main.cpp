26f6630f66a8b3a3de792f652eb5e754f55dc442
mutsuki <mutsuki@momo86.net>
2023-12-30 07:09:11 +0000

    Add blocking

--- 01-vectoradd/main.cpp
+++ 01-vectoradd/main.cpp
@@ -33,6 +33,8 @@ int main() {
   std::printf("device = %s\n",
               queue.get_device().get_info<sycl::info::device::name>().c_str());
 
+  constexpr std::uint32_t block_N = 2;
+
   auto command_group = [&](sycl::handler& handler) {
     constexpr auto read_t  = sycl::access::mode::read;
     constexpr auto write_t = sycl::access::mode::write;
@@ -41,16 +43,31 @@ int main() {
     auto b = vec_b.get_access<read_t >(handler);
     auto c = vec_c.get_access<write_t>(handler);
 
-    handler.parallel_for(vec_range, [=](sycl::id<1> i) {c[i] = a[i] + b[i];});
+    handler.parallel_for(sycl::range(N / block_N), [=](sycl::id<1> tid) {
+      compute_t buf_a[block_N];
+      compute_t buf_b[block_N];
+      for (std::uint32_t i = 0; i < block_N; i++) {
+        buf_a[i] = a[tid * block_N + i];
+        buf_b[i] = b[tid * block_N + i];
+      }
+      for (std::uint32_t i = 0; i < block_N; i++) {
+        c[tid * block_N + i] = buf_a[i] + buf_b[i];
+      }
+    });
   };
-
-  const auto start_clock = std::chrono::system_clock::now();
   queue.submit(command_group);
+  queue.wait();
+
+  constexpr std::uint32_t test_c = 100;
+  const auto start_clock = std::chrono::system_clock::now();
+  for (std::uint32_t i = 0; i < test_c; i++) {
+    queue.submit(command_group);
+  }
   queue.wait();
   const auto end_clock = std::chrono::system_clock::now();
   const auto elapsed_time = std::chrono::duration_cast<std::chrono::nanoseconds>(end_clock - start_clock).count() * 1e-9;
   std::printf("bw = %e [GB/s]\n",
-              sizeof(compute_t) * 3 * N / elapsed_time * 1e-9
+              sizeof(compute_t) * 3 * N * test_c / elapsed_time * 1e-9
              );
 
   {
